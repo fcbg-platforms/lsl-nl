7b2e36906e83603ba03339eaf96f77249621b011
Gwénaël Birot <gwenael.birot@fcbg.ch>
2022-12-01 12:04:58 +0100

    Initial commit

---
+++ NL_mockAmplifier.py
@@ -0,0 +1,56 @@
+import socket
+import time
+import struct
+import random
+import numpy as np
+import psychtoolbox as ptb
+
+def startAmp(nChannels=64,sr=.001,duration=60):
+	UDP_IP = "127.0.0.1"
+	UDP_PORT = 5005 
+	
+	sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	
+	sampleCount=0
+	size = 68+nChannels*4 + 4
+	
+	f0 = 4
+	
+	print('Sending data...')
+	t0 = ptb.GetSecs()
+	while(True):
+		t1 = ptb.GetSecs()
+		stx = 0
+		id = sampleCount
+		ts = ptb.GetSecs() - t0
+		status = 0
+		ttl = 0
+		packet = bytearray(size)
+		
+		# pack header
+		struct.pack_into('!3Id2I10I',packet,0,stx,id,size,ts,status,ttl,*range(10))
+		
+		# pack data
+		# data = np.arange(nChannels) + sampleCount*nChannels	
+		# ,dtype= np.int32
+		t = sampleCount*sr
+		data = (np.sin(np.ones(nChannels)*2*np.pi*sampleCount*sr*f0)*1e6).astype(np.int32)
+		
+		struct.pack_into('!' + str(nChannels) + 'i', packet, 68,*data)
+		
+		# pack footer
+		struct.pack_into('!I', packet, 68 + nChannels*4, 0)
+		
+		sock.sendto(packet,(UDP_IP, UDP_PORT))
+		sampleCount+=1
+		
+		ptb.WaitSecs('UntilTime',t1+sr)
+		if (ptb.GetSecs() - t0) > duration:
+			break
+	
+	print('Sent ' + str(sampleCount) + ' samples in ' + str(ts) + ' seconds')
+
+	
+
+if __name__ == '__main__': 
+ 	startAmp(nChannels=64, sr = .001, duration = 120)
---
+++ nl2lsl.py
@@ -0,0 +1,77 @@
+import argparse
+import socket
+import struct
+import numpy as np
+from pylsl import StreamInfo, StreamOutlet, local_clock
+
+def nl2lsl(UDP_IP="192.168.3.100", UDP_PORT=26090, nBoards=2, srate=1024):
+	# UDP_IP = "127.0.0.1"
+	# UDP_PORT = 5005 
+	# nBoards = 2 # number neuralynx board
+	# srate = 1000 # sampling rate
+
+	# size in bytes
+	header_size = 68
+	data_size = 128 # data size per board
+	footer_size = 4
+
+	packet_size = header_size + 4*32*nBoards + footer_size # packet size in bytes
+	nChannels = nBoards*32 # number of channels streamed
+
+	# open a socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+	sock.settimeout(None)
+	sock.bind((UDP_IP, UDP_PORT))
+
+	# create LSL stream
+	info = StreamInfo('NeuraLynx', 'EEG', nChannels, srate, 'float32', 'myuid34234')
+	info.desc().append_child_value("manufacturer", "NeuraLynx")
+	channels = info.desc().append_child("channels")
+
+	for n in range(nChannels):
+		channels.append_child("channel") \
+			.append_child_value("label", str(n+1)) \
+			.append_child_value("unit", "microvolts") \
+			.append_child_value("type", "EEG") \
+			.append_child_value("ref", str(0))
+
+	outlet = StreamOutlet(info)
+
+	print('Waiting for data...')
+	firstPacketReceived = False
+	while True:
+		# get data from neuralynx
+		try:
+			packet, addr = sock.recvfrom(packet_size)
+		except socket.timeout:
+			print('Timeout error: no data received during the last 2 seconds')
+			break		
+
+		if not firstPacketReceived:
+			print('Receiving data from ' +addr[0]+':'+str(addr[1])+'... [press Ctrl+c to stop]')
+			sock.settimeout(2)
+		
+		firstPacketReceived = True
+		
+		header = struct.unpack_from('!3Id2I10I',packet,offset = 0)
+		data = struct.unpack_from('!' + str(nChannels) +'i',packet,offset = header_size)
+		footer = struct.unpack_from('!I',packet,offset = header_size+nChannels*4)
+		
+		# print(header[1])
+		# print(len(data))
+		
+		# push data in LSL stream
+		outlet.push_sample(np.array(data)/1e5)
+	
+		
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('ip', help='IP address to which data are received')
+	parser.add_argument('port', help='port number to which data are received', type=int)
+	parser.add_argument('nBoards', help='number of boards of the Neuralynx system',type=int)
+	parser.add_argument('srate', help='sampling rate',type=int)
+	args = parser.parse_args()
+	
+	nl2lsl(args.ip, args.port, args.nBoards, args.srate)
+	
+	
