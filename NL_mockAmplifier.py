636714c4bb2a1f9ff3153281fa70bbe6c3f1b8bc
gbirot <gwenael.birot@fcbg.ch>
2023-05-16 10:47:03 +0200

    ADD decimation

--- NL_mockAmplifier.py
+++ NL_mockAmplifier.py
@@ -7,7 +7,7 @@ import psychtoolbox as ptb
 
 def startAmp(nChannels=64,sr=.001,duration=60):
 	UDP_IP = "127.0.0.1"
-	UDP_PORT = 5005 
+	UDP_PORT = 5000 
 	
 	sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 	
@@ -28,7 +28,7 @@ def startAmp(nChannels=64,sr=.001,duration=60):
 		packet = bytearray(size)
 		
 		# pack header
-		struct.pack_into('!3Id2I10I',packet,0,stx,id,size,ts,status,ttl,*range(10))
+		struct.pack_into('<3Id2I10I',packet,0,stx,id,size,ts,status,ttl,*range(10))
 		
 		# pack data
 		# data = np.arange(nChannels) + sampleCount*nChannels	
@@ -36,10 +36,10 @@ def startAmp(nChannels=64,sr=.001,duration=60):
 		t = sampleCount*sr
 		data = (np.sin(np.ones(nChannels)*2*np.pi*sampleCount*sr*f0)*1e6).astype(np.int32)
 		
-		struct.pack_into('!' + str(nChannels) + 'i', packet, 68,*data)
+		struct.pack_into('<' + str(nChannels) + 'i', packet, 68,*data)
 		
 		# pack footer
-		struct.pack_into('!I', packet, 68 + nChannels*4, 0)
+		struct.pack_into('<I', packet, 68 + nChannels*4, 0)
 		
 		sock.sendto(packet,(UDP_IP, UDP_PORT))
 		sampleCount+=1
@@ -53,4 +53,4 @@ def startAmp(nChannels=64,sr=.001,duration=60):
 	
 
 if __name__ == '__main__': 
- 	startAmp(nChannels=64, sr = .001, duration = 120)
+ 	startAmp(nChannels=64, sr = .0001, duration = 120)
